924a7ba54ff99ecc212de670e65112135edb1f9d
Shay Craig <71286979+craigs40@users.noreply.github.com>
2021-08-09 13:58:48 -0400

    Update models, refactor friendship spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,15 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :friendships
-  has_many :inverse_friendships, class_name: :friendship, foreign_key: :friend_id
-  has_many :sent_friendships, foreign_key: :sender_id, class_name: :Friendship
+  has_many :inverted_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
+  has_many :friend_requests, through: :inverted_friendships
   has_many :received_friendships, foreign_key: :receiver_id, class_name: :Friendship
   has_many :requesting_friends, through: :received_friendships
   has_many :requested_friends, through: :sent_friendships
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: 'Friendship'
+  has_many :friends, through: :confirmed_friendships
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
+  has_many :pending_friends, through: :pending_friendships, source: :friend
 
   def pending_friends
     friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
@@ -34,6 +38,10 @@ class User < ApplicationRecord
 
   def friend?(user)
     friends.include?(user)
+  end
+
+  def friends_and_own_posts
+    Post.where(user: (friends.to_a << self))
   end
 end
 
--- spec/friendship_spec.rb
+++ spec/friendship_spec.rb
@@ -1,12 +1,8 @@
-RSpec.describe Friendship do
-  describe 'associations' do
-    it 'belongs to friend' do
-      friendship = Friendship.reflect_on_association(:friend)
-      expect(friendship.macro).to eq(:belongs_to)
-    end
-    it 'belongs to user' do
-      friendship = Friendship.reflect_on_association(:user)
-      expect(friendship.macro).to eq(:belongs_to)
-    end
+RSpec.describe 'Friend request' do
+  it 'creates a friend request' do
+    send_friend_request
+    expect(page).to have_content('Friend request sent')
+    expect(User.first.pending_friends.first).to eq(User.last)
+    expect(User.last.friend_requests.first).to eq(User.first)
   end
 end
